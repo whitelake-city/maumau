dc6b735ebfe46d03577571467fb5a0eaf023d118
Matthias Hartmann <hmatthi@amazon.com>
2019-02-16 18:33:29 +0100

    styling welcome screen

--- browser/src/lobby/lobby.jsx
+++ browser/src/lobby/lobby.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Container from "reactstrap/es/Container";
-import { Row } from "reactstrap";
+import {Row} from "reactstrap";
 import Col from "reactstrap/es/Col";
 import Button from "reactstrap/es/Button";
 
 export class Lobby extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         if (!props.spiel.id) {
             this.props.sucheSpiel(this.props.match.params.spielerId)
         } else {
             this.props.api.warteAufSpielStart(this.props.spiel.id, (spiel)=>{
-                this.props.starteSpiel(spiel)
+                this.props.starteSpiel(spiel);
                 this.props.history.push(`/mau-mau/${spiel.spieler.id}`)
-            })
+            });
             this.props.api.warteAufSpielerBereit(this.props.spiel.id, this.props.aktualisiereSpiel)
         }
     }
 
     spielerIstBereit = () => {
         this.props.api.spielerIstBereit(this.props.spiel.spieler.id,this.props.aktualisiereSpiel)
-    }
+    };
 
     render() {
         return (
-            <Container>
+            <Container className={"lobby-wrapper"}>
                 <Row>
                     <Col>
                         <h1>Willkommen in der Lobby {this.props.spiel.spieler.name}!</h1>
@@ -35,20 +35,23 @@ export class Lobby extends Component {
                         <h3>Spieler:</h3>
                     </Col>
                 </Row>
+                <Row key={this.props.spiel.spieler.name}>
+                    <Col>{this.props.spiel.spieler.name}</Col>
+                    <Col>{this.props.spiel.spieler.bereit ? "bereit" : "wartet"}</Col>
+                </Row>
                 {this.props.spiel.mitspieler.map(
                     (spieler) => {
                         return (
                             <Row key={spieler.name}>
                                 <Col>{spieler.name}</Col>
-                                <Col>{spieler.bereit===true?'bereit':'nicht bereit'}</Col>
+                                <Col>{spieler.bereit ? "bereit" : "wartet"}</Col>
                             </Row>
                         );
                     }
                 )}
                 <Row>
-                    <Col><Button color="success" onClick={this.spielerIstBereit} disabled={this.props.spiel.spieler.bereit}>Spiel starten</Button> </Col>
+                    <Col><Button color="info" disabled={this.props.spiel.spieler.bereit} onClick={this.spielerIstBereit}>Spiel starten</Button></Col>
                 </Row>
-
             </Container>
         );
     }
--- browser/src/lobby/welcome.jsx
+++ browser/src/lobby/welcome.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Button, Col, Container, Input, InputGroup, Row} from 'reactstrap';
+import {Button, Input, InputGroup} from 'reactstrap';
 import {KartenRueckseite} from "../maumau/komponenten/karten/kartenRueckseite";
 
 export class Welcome extends Component {
@@ -17,9 +17,9 @@ export class Welcome extends Component {
     };
 
     erstelleSpieler = (event) => {
-        event.preventDefault()
+        event.preventDefault();
         this.props.api.erstelleSpieler(this.state.spielername, this.geheInDieLobby)
-    }
+    };
 
     geheInDieLobby = (spieler) => {
         this.props.history.push(`/lobby/${spieler.id}`)
@@ -28,23 +28,18 @@ export class Welcome extends Component {
     render() {
         return (
             <div className={"welcome"}>
-                <Container>
-                    <Row>
-                        <KartenRueckseite/>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <form onSubmit={this.erstelleSpieler}>
-                                <InputGroup>
-                                    <Input placeholder="Spielername" onChange={this.setzeSpielernamen} required/>
-                                </InputGroup>
-                                <InputGroup>
-                                    <Button color="success" disabled={this.state.spielername.length < 1}>Neues Siel erstellen</Button>
-                                </InputGroup>
-                            </form>
-                        </Col>
-                    </Row>
-                </Container>
+                <div className={"titel"}>
+                    Mau<br/>Mau
+                </div>
+                <KartenRueckseite/>
+                <form onSubmit={this.erstelleSpieler} className={"spielerdaten"}>
+                    <InputGroup>
+                        <Input placeholder="Spielername" onChange={this.setzeSpielernamen} required/>
+                    </InputGroup>
+                    <InputGroup>
+                        <Button color="primary" disabled={this.state.spielername.length < 1}>Neues Siel erstellen</Button>
+                    </InputGroup>
+                </form>
             </div>
         )
     }
