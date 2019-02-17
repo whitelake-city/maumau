035ddc47452f8914d947d72df004e10d4cb2d19d
Matthias Hartmann <hmatthi@amazon.com>
2019-02-17 16:02:26 +0100

    renamed svg files and adjusted spielfeld to use new state

--- browser/src/maumau/maumau.jsx
+++ browser/src/maumau/maumau.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {SpielerKarten} from "./komponenten/spielerkarten/spieler";
 import {Mitspieler} from "./komponenten/mitspielerkarten/mitspieler";
 import {KartenStapel} from "./komponenten/kartenstapel/kartenstapel";
-import {ART, WERT} from "./konstanten";
 
 export class MauMau extends React.Component {
     constructor(props) {
@@ -10,65 +9,10 @@ export class MauMau extends React.Component {
         if (!props.spiel.id) {
             this.props.sucheSpiel(this.props.match.params.spielerId)
         }
-        this.state = {
-            mitspieler: [
-                {
-                    name: "Horst",
-                    karten: 2
-                },
-                {
-                    name: "Peter",
-                    karten: 11
-                },
-                {
-                    name: "Gertrude",
-                    karten: 8
-                },
-            ],
-            kartenstapel: {
-                kartenanzahl: 10,
-                aktuellSichtbareKarte: {
-                    art: ART.PIK,
-                    wert: WERT.ASS
-                }
-            },
-            spieler: {
-                id: this.props.spiel.spieler.id,
-                name: this.props.spiel.spieler.name,
-                karten: [
-                    {
-                        art: ART.KARO,
-                        wert: WERT.ASS
-                    },
-                    {
-                        art: ART.PIK,
-                        wert: WERT.BUBE
-                    },
-                    {
-                        art: ART.HERZ,
-                        wert: WERT.DAME
-                    },
-                    {
-                        art: ART.KREUZ,
-                        wert: WERT.KOENIG
-                    },
-                ]
-            },
-        };
-
     }
 
     componentDidMount() {
         // TODO load all necessary information and sort opponents by name lexicographically for a steady order or provide an arbitrary id
-        this.setState(this.state.mitspieler.sort((a, b) => {
-            if (a.name < b.name) {
-                return -1;
-            }
-            if (a.name > b.name) {
-                return 1;
-            }
-            return 0;
-        }))
     }
 
     karteGewaehlt = (position) => {
@@ -86,13 +30,13 @@ export class MauMau extends React.Component {
     render() {
         return (
             <div className={"maumau"}>
-                <Mitspieler mitspieler={this.state.mitspieler} />
+                <Mitspieler mitspieler={this.props.spiel.mitspieler} />
                 <KartenStapel
-                    kartenanzahl={this.state.kartenstapel.kartenanzahl}
-                    aktuellSichtbareKarte={this.state.kartenstapel.aktuellSichtbareKarte}
+                    // aktuellSichtbareKarte={this.props.spiel.stapel.karte}
+                    aktuellSichtbareKarte={ { art: "pik", wert: "b"} }
                 />
                 <SpielerKarten
-                    spieler={this.state.spieler}
+                    spieler={this.props.spiel.spieler}
                     beiClick={this.karteGewaehlt}
                 />
             </div>
