132f9f153fb3517af91d609eeeb26de2631ee441
whitelake-city <spamleogerber@gmail.com>
2019-02-17 17:53:35 +0100

    change styling if player is not next

--- browser/src/maumau/komponenten/spielerkarten/spieler.jsx
+++ browser/src/maumau/komponenten/spielerkarten/spieler.jsx
@@ -3,10 +3,11 @@ import {KartenVorderseite} from "../karten/kartenVorderseite";
 
 export class SpielerKarten extends Component {
     render() {
+        let kartenKlasse = this.props.spieler.amZug?"karten":"karten-deaktiviert"
         return (
             <div className={"spieler"}>
                 <h1 className={"name"}>{this.props.spieler.name}</h1>
-                <div className={"karten"}>
+                <div className={kartenKlasse}>
                     {
                         this.props.spieler.karten.map(
                             (aktuelleKarte, idx) => (
