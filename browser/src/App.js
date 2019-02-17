8b22a49ce4671346e381a87057ce8decd4ea4f8f
Matthias Hartmann <hmatthi@amazon.com>
2019-02-17 16:42:39 +0100

    lift callback for played card up to app.js

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,89 +1,110 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css'
-import { Welcome } from './lobby/welcome'
-import { MauMau } from "./maumau/maumau";
-import { Api, ApiComponent } from './api/api'
-import { Lobby } from "./lobby/lobby";
+import {Welcome} from './lobby/welcome'
+import {MauMau} from "./maumau/maumau";
+import {Api, ApiComponent} from './api/api'
+import {Lobby} from "./lobby/lobby";
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      api: new Api(),
+    constructor(props) {
+        super(props);
+        this.state = {
+            api: new Api(),
 
-      spiel: {
-        mitspieler: [],
-        spieler: {
-          id: '',
-          name: ''
-        }
-      }
-    };
-    this.state.api.connect()
-  }
-
-  setzeSpielerZurueck = () => {
-    if (this.state.spiel.spieler.id !== '') {
-      this.setState({
-        spiel: {
-          mitspieler: [],
-          spieler: {
-            id: '',
-            name: ''
-          }
-        }
-      })
+            spiel: {
+                mitspieler: [],
+                gelegt: {
+                    wert: 'b',
+                    art: 'pik'
+                },
+                spieler: {
+                    id: '',
+                    name: ''
+                }
+            }
+        };
+        this.state.api.connect()
     }
-  }
 
-  sucheSpiel = (spielerId) => {
-    this.state.api.sucheSpiel(spielerId, this.aktualisiereSpiel)
-  }
+    setzeSpielerZurueck = () => {
+        if (this.state.spiel.spieler.id !== '') {
+            this.setState({
+                              spiel: {
+                                  mitspieler: [],
+                                  spieler: {
+                                      id: '',
+                                      name: ''
+                                  }
+                              }
+                          })
+        }
+    };
 
-  aktualisiereSpiel = (spiel) => {
-    this.setState({ spiel: spiel })
-  }
+    sucheSpiel = (spielerId) => {
+        this.state.api.sucheSpiel(spielerId, this.aktualisiereSpiel)
+    };
 
-  starteSpiel = (spiel) => {
-    this.setState({ spiel: spiel })
-  }
+    aktualisiereSpiel = (spiel) => {
+        this.setState({ spiel: spiel })
+    };
 
-  render() {
-    return (
-      <Router>
-        <div className={"router"}>
-          <Route exact path="/" component={props => <Welcome {...props} setzeSpielerZurueck={this.setzeSpielerZurueck} api={this.state.api} />} />
-          <Route
-            path="/lobby/:spielerId"
-            component={
-              props =>
-                <Lobby
-                  {...props}
-                  sucheSpiel={this.sucheSpiel}
-                  starteSpiel={this.starteSpiel}
-                  aktualisiereSpiel={this.aktualisiereSpiel}
-                  api={this.state.api}
-                  spiel={this.state.spiel}
-                />
-            }
-          />
-          <Route
-            path="/mau-mau/:spielerId"
-            component={
-              props =>
-                <MauMau
-                  {...props}
-                  spiel={this.state.spiel}
-                  sucheSpiel={this.sucheSpiel}
-                />
-            }
-          />
-          <Route path="/api" component={props => <ApiComponent {...props} api={this.state.api} />} />
-        </div>
-      </Router>
-    );
-  }
+    starteSpiel = (spiel) => {
+        this.setState({ spiel: spiel })
+    };
+
+    karteGewaehlt = (position) => {
+        // TODO send played card to server
+        let gespielteKarte = this.state.spiel.spieler.karten[position];
+
+        this.state.spiel.spieler.karten.splice(position, 1); // TODO remove me. not necessary upon subscription
+        this.setState({ gelegt: gespielteKarte })
+    };
+
+    render() {
+        return (
+            <Router>
+                <div className={"router"}>
+                    <Route exact path="/"
+                           component={props =>
+                               <Welcome
+                                   {...props}
+                                   setzeSpielerZurueck={this.setzeSpielerZurueck}
+                                   api={this.state.api}
+                               />
+                           }
+                    />
+                    <Route
+                        path="/lobby/:spielerId"
+                        component={
+                            props =>
+                                <Lobby
+                                    {...props}
+                                    sucheSpiel={this.sucheSpiel}
+                                    starteSpiel={this.starteSpiel}
+                                    aktualisiereSpiel={this.aktualisiereSpiel}
+                                    api={this.state.api}
+                                    spiel={this.state.spiel}
+                                />
+                        }
+                    />
+                    <Route
+                        path="/mau-mau/:spielerId"
+                        component={
+                            props =>
+                                <MauMau
+                                    {...props}
+                                    spiel={this.state.spiel}
+                                    sucheSpiel={this.sucheSpiel}
+                                    karteGewaehlt={this.karteGewaehlt}
+                                />
+                        }
+                    />
+                    <Route path="/api" component={props => <ApiComponent {...props} api={this.state.api}/>}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- browser/src/maumau/maumau.jsx
+++ browser/src/maumau/maumau.jsx
@@ -15,29 +15,18 @@ export class MauMau extends React.Component {
         // TODO load all necessary information and sort opponents by name lexicographically for a steady order or provide an arbitrary id
     }
 
-    karteGewaehlt = (position) => {
-        // TODO send picked card to server
-        var karte = this.state.spieler.karten[position];
-
-        this.state.spieler.karten.splice(position, 1);
-        this.setState({
-            kartenstapel: {
-                aktuellSichtbareKarte: karte
-            }
-        })
-    };
 
     render() {
         return (
             <div className={"maumau"}>
                 <Mitspieler mitspieler={this.props.spiel.mitspieler} />
                 <KartenStapel
-                    // aktuellSichtbareKarte={this.props.spiel.stapel.karte}
+                    // aktuellSichtbareKarte={this.props.spiel.gelegt}
                     aktuellSichtbareKarte={ { art: "pik", wert: "b"} }
                 />
                 <SpielerKarten
                     spieler={this.props.spiel.spieler}
-                    beiClick={this.karteGewaehlt}
+                    beiClick={this.props.karteGewaehlt}
                 />
             </div>
         )
