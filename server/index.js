ba5295ddb09f582c3f9bed34994fe9669cd7c0ac
whitelake-city <spamleogerber@gmail.com>
2019-02-22 20:41:48 +0100

    add example static html

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const io = require('socket.io')(8000)
+const socketIO = require('socket.io')
 const Db = require('./db/db')
 const Game = require('./game/game')
+const Koa = require('koa')
+const http = require('http')
+const serve = require('koa-static');
 
 class Server {
     constructor() {
@@ -10,9 +13,27 @@ class Server {
     start() {
         this.db = new Db();
         this.game = new Game()
-        this.db.connect(()=>{
-            io.on('connection', (client) => {
-                let game = new Game(this.db,client,io)
+
+        // create Koa app instance
+        this.app = new Koa();
+        this.app.use(serve('./public/'));
+
+        // manually create http server and override app.listen
+        // so we can start both servers together
+        this.app.server = http.createServer(this.app.callback()).listen(8000);
+        this.app.listen = (...args) => {
+            this.app.server.listen.call(this.app.server, ...args);
+            return this.app.server;
+        };
+
+        // create Socket.io app instance
+        this.app.io = socketIO(this.app.server, {});
+
+        
+
+        this.db.connect(() => {
+            this.app.io.on('connection', (client) => {
+                let game = new Game(this.db, client, this.app.io)
                 game.start()
             })
         })
@@ -20,4 +41,4 @@ class Server {
 }
 
 const server = new Server()
-server.start()
+server.start({ port: 3000 })
